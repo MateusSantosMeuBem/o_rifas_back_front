af678c5b87c1fa1816f904e378bc7d680d59ac3f
Mateus Santos <a.mateussm1.5@gmail.com>
2023-06-21 16:27:03 -0300

    *.jsx files to *.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Title from './Components/Title';
+
 import './App.css'
 
 const App = () => {
@@ -5,6 +7,7 @@ const App = () => {
   return (
     <>
       {/* Título */}
+      <Title />
       {/* Banner */}
       {/* Números */}
       {/* Dados de comprar */}
