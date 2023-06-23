16c4429c35f76e5a92570a725da88123f1f23d16
Elian-beep <elianoliver101@gmail.com>
2023-06-23 11:37:11 -0300

    :racehorse: perf: added import performance

--- src/Pages/Raffle/index.tsx
+++ src/Pages/Raffle/index.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 
-import Banner from "../../Components/Banner"
-import NumberButton from "../../Components/Numbers/NumberButton"
-import Title from "../../Components/Title"
-import { SellerNumbers, SellerProps, defaultSeller } from "../../props/SellerProps";
+import Banner from "@/Components/Banner"
+import NumberButton from "@/Components/Numbers/NumberButton"
+import Title from "@/Components/Title"
+import { SellerNumbers, SellerProps, defaultSeller } from "@/props/SellerProps";
 
 import { useParams } from 'react-router-dom';
 
